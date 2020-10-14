a0ffcfd05dac652e78d0b26ed8ffbaa04706b919
Roshan Jha <kartohajha@gmail.com>
2020-10-14 17:13:35 +0530

    add bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,20 @@
+const bubbleSort = (arr) => {
+  let stepsCount = arr.length - 1;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < stepsCount; i += 1) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+    stepsCount -= 1;
+  } while (swapped);
+
+  return arr;
+};
+
+console.log(bubbleSort([1, -1]))
